aeebdf9065ee044be568896b85e98eb21aaf5fe4
shede3020 <275648208@qq.com>
2016-01-23 19:56:11 +0800

    鼠标拖拽

--- 20160120_全日制第三周第二天/js/utils.js
+++ 20160120_全日制第三周第二天/js/utils.js
@@ -22,21 +22,22 @@ var utils = {
 };
 
 //getCss:Gets the style of the current element that is calculated by the browser
-utils.getCss = function (curEle, attr) {
+utils.getCss = function (curEle, attr) {//curEle 当前要操作的元素（比如div li）  attr是div里的属性。
     var val = reg = null;
     if ("getComputedStyle" in window) {
         val = window.getComputedStyle(curEle, null)[attr];
     } else {
         if (attr === "opacity") {
             val = curEle.currentStyle["filter"];
+            //opacity; 这里的是ie6，7，8
             reg = /^alpha\(opacity=(\d+(\.\d+)?)\)$/;
-            val = reg.test(val) ? reg.exec(val)[1] / 100 : 1;
+            val = reg.test(val) ? reg.exec(val)[1] / 100 : 1; //没捕获到就是1，有可能没加透明度；
         } else {
             val = curEle.currentStyle[attr];
         }
     }
-    reg = /^-?\d+(\.\d+)?(px|pt|em|rem)?$/;
-    return reg.test(val) ? parseFloat(val) : val;
+    reg = /^-?\d+(\.\d+)?(px|pt|em|rem)?$/;  //去单位
+    return reg.test(val) ? parseFloat(val) : val; //去单位，
 };
 
 //offset:Gets the offset of the current element distance body
